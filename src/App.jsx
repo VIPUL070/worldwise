11d464151b91ffe9a4220b9941d7bec8cd9a4177
VIPUL070 <sutharvipul007@gmail.com>
2025-10-11 09:59:25 +0530

    Adding fake API for project and implementing Nested Routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Homepage from './pages/Homepage'
 import PageNotFound from './pages/PageNotFound'
 import AppLayout from "./pages/AppLayout"
 import Login from './pages/Login';
+import CityList from "./components/CityList"
 
 function App() {
   return (
@@ -13,7 +14,14 @@ function App() {
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
         <Route path="login"  element={<Login />} />
-        <Route path='app' element={<AppLayout />} />
+        <Route path='app' element={<AppLayout />} >
+
+           <Route index element={<CityList />}/>
+           <Route path="cities" element={<CityList />}/>
+           <Route path="countries" element={<p>List of countries</p>}/>
+           <Route path="form" element={<p>Form</p>}/>
+           
+        </Route>
         <Route path='*' element={<PageNotFound />} />
       </Routes>
   )
