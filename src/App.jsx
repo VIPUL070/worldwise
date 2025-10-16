db2832ad38a73b27936c130e1b45a41ad30b6ab2
VIPUL070 <sutharvipul007@gmail.com>
2025-10-16 15:52:52 +0530

    Using Context API for Global state management to build City component

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,20 @@
-import { Routes, Route } from "react-router-dom"
-import { useEffect, useState } from "react"
-import { Navigate } from "react-router-dom"
-import Product from './pages/Product'
-import Pricing from './pages/Pricing'
-import Homepage from './pages/Homepage'
-import PageNotFound from './pages/PageNotFound'
-import AppLayout from "./pages/AppLayout"
+import { Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from "./pages/AppLayout";
 import Login from './pages/Login';
-import CityList from "./components/CityList"
-import CountryList from "./components/CountryList"
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 import Form from './components/Form';
 import City from "./components/City";
-
-const BASE_URL = 'http://localhost:8000';
+import { CitiesProvider} from "./contexts/CitiesContext";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert('There was an error loading the data....');
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchCities();
-
-  }, []);
-
   return (
+    <CitiesProvider>
     <Routes>
       <Route path='/' element={<Homepage />} />
       <Route path='product' element={<Product />} />
@@ -45,15 +23,16 @@ function App() {
       <Route path='app' element={<AppLayout />} >
 
         <Route index element={<Navigate replace to='cities'/>} />
-        <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
+        <Route path="cities" element={<CityList />} />
         <Route path='cities/:id' element={<City />}/>
-        <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
+        <Route path="countries" element={<CountryList />} />
         <Route path="form" element={<Form />} />
 
       </Route>
       <Route path='*' element={<PageNotFound />} />
     </Routes>
+    </CitiesProvider>
   )
 }
 
-export default App
+export default App;
