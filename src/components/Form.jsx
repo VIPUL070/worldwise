584235fac326274b1b0776a00b000ff7c3ba9709
VIPUL070 <sutharvipul007@gmail.com>
2025-10-21 11:21:37 +0530

    Adding new city and deleting city from API

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,16 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 import { useEffect, useState } from "react";
 import { useUrlPosition } from "../hooks/useUrlPosition";
+import { useCities } from "../contexts/CitiesContext";
+import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 import styles from "./Form.module.css";
 import BackButton from "./BackButton";
 import Message from './Message';
 import Spinner from './Spinner';
-
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+ 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
     .toUpperCase()
@@ -18,23 +22,25 @@ export function convertToEmoji(countryCode) {
 const BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
  
 function Form() {
+  const {addCity , isLoading} = useCities();
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
-  const [mapLat , mapLng] = useUrlPosition();
+  const [lat , lng] = useUrlPosition();
   const [isLoadingGeocoding , setIsLoadingGeocoding] = useState(false);
   const [emoji , setEmoji] = useState('');
   const [geocodingError , setGeocodingError] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
-      if(!mapLat && !mapLng) return ;
+      if(!lat && !lng) return ;
 
       async function fetchNewCity() {
         try {
           setIsLoadingGeocoding(true);
           setGeocodingError('');
-          const res = await fetch(`${BASE_URL}?latitude=${mapLat}&longitude=${mapLng}`);
+          const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
           const data = await res.json();
           if(!data.countryCode) throw new Error("That doesn't seem to be a city. Click somewhere else");
           setCityName(data.city || data.locality || '');
@@ -49,14 +55,31 @@ function Form() {
       }
 
       fetchNewCity();
-  },[mapLat,mapLng])
+  },[lat,lng])
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    if(!cityName || !date) return;
+
+    const newCity = 
+    {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position :{ lat , lng},
+    }
+    await addCity(newCity);
+    navigate("/app/cities");
+  }
 
   if(isLoadingGeocoding) return <Spinner />
-  if(!mapLat && !mapLng) return <Message message="Start by clicking on the map"/>
+  if(!lat && !lng) return <Message message="Start by clicking on the map"/>
   if(geocodingError) return <Message message={geocodingError}/>
 
   return (
-    <form className={styles.form}>
+    <form className={`${styles.form} ${isLoading ? styles.loading : ""}`} onSubmit={handleSubmit}>
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -69,10 +92,11 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
-          id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
+        <DatePicker 
+        id="date"
+        onChange={(date) => setDate(date)}
+        selected={date}
+        dateFormat="dd/MM/yyyy"
         />
       </div>
 
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -37,13 +37,52 @@ function CitiesProvider({ children }) {
         }
     }
 
+    async function addCity(newCity) {
+        try {
+            setIsLoading(true);
+            const res = await fetch(`${BASE_URL}/cities`,
+                {
+                    method: "POST",
+                    body: JSON.stringify(newCity),
+                    headers: { "Content-Type": "application/json" },
+                }
+            );
+            const data = await res.json();
+            setCities((cities) => [...cities, data]);
+
+        } catch {
+            alert('There was an error adding the city....');
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    async function deleteCity(id) {
+        try {
+            setIsLoading(true);
+            await fetch(`${BASE_URL}/cities/${id}`,
+                {
+                    method: "DELETE",
+                }
+            );
+            setCities((cities) => cities.filter((city) => city.id !== id));
+
+        } catch {
+            alert('There was an error deleting the city....');
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
         <CitiesContext.Provider
             value={{
                 cities,
                 isLoading,
                 currentCity,
-                getCity
+                getCity,
+                addCity,
+                deleteCity
             }}
         >
             {children}
